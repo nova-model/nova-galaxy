3f645b05c983c6ee5e4defecc63a3dc67a35680b
Gregory Cage <cagege@ornl.gov>
2024-10-31 15:51:44 -0400

    Change typing and naming of parameters inputs to explicitly allow any type

--- src/nova/galaxy/parameters.py
+++ src/nova/galaxy/parameters.py
@@ -2,8 +2,6 @@
 
 from typing import Any, Dict
 
-from .dataset import Dataset
-
 
 class Parameters:
     """Specialized map wrapper used as an input to a Galaxy tool."""
@@ -11,12 +9,12 @@ class Parameters:
     def __init__(self) -> None:
         self.inputs: Dict[str, Any] = {}
 
-    def add_input(self, name: str, dataset: Dataset) -> None:
-        self.inputs[name] = dataset
+    def add_input(self, name: str, value: Any) -> None:
+        self.inputs[name] = value
 
-    def change_input_value(self, name: str, new_dataset: Dataset) -> None:
+    def change_input_value(self, name: str, new_value: Any) -> None:
         if self.inputs[name]:
-            self.inputs[name] = new_dataset
+            self.inputs[name] = new_value
 
     def remove_input(self, name: str) -> None:
         self.inputs.pop(name)
