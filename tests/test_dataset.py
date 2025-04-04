7f666b5fbde445a490a9c33a9c6d965b70829962
Gregory Cage <cagege@ornl.gov>
2025-04-04 12:11:16 -0400

    Expand dataset functionality with manual content setting and file type metadata

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -12,6 +12,16 @@ def test_dataset_upload(nova_instance: Connection) -> None:
         assert input.get_content() is not None
 
 
+def test_dataset_set_content_upload(nova_instance: Connection) -> None:
+    with nova_instance.connect() as connection:
+        store = connection.create_data_store(name="nova_galaxy_testing")
+        input = Dataset()
+        # File type is optional
+        input.set_content(content="this is some content, that I'm setting", file_type=".txt")
+        input.upload(store)
+        assert input.get_content() is not None
+
+
 def test_dataset_collection_upload(nova_instance: Connection) -> None:
     # TODO: Dataset collection uploading needs to be implemented
     pass
