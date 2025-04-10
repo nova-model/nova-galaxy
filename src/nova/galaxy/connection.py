af2b2d5e899b9aaf0e525d0e3bbd9ac828dc9573
Gregory Cage <cagege@ornl.gov>
2025-04-10 15:52:48 -0400

    strealime connection and data store management

--- src/nova/galaxy/connection.py
+++ src/nova/galaxy/connection.py
@@ -1,7 +1,6 @@
 """The NOVA class is responsible for managing interactions with a Galaxy server instance."""
 
-from contextlib import contextmanager
-from typing import Generator, List, Optional
+from typing import Any, List, Optional
 
 from bioblend import galaxy
 
@@ -29,27 +28,49 @@ class ConnectionHelper:
     be automatically purged after connection is closed, unless Datastore.persist() is called for that store.
     """
 
-    def __init__(self, galaxy_instance: galaxy.GalaxyInstance, galaxy_url: str):
+    def __init__(self, galaxy_instance: galaxy.GalaxyInstance, galaxy_url: Optional[str]):
         self.galaxy_instance = galaxy_instance
         self.galaxy_url = galaxy_url
         self.datastores: List[Datastore] = []
 
+    def __enter__(self) -> Any:
+        """Enter method for use with "with" keyword."""
+        return self
+
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        """Exit method for use with "with" keyword."""
+        self.close()
+
     def create_data_store(self, name: str) -> Datastore:
+        """Creates a datastore with the given name."""
+        return self.use_data_store(name=name, create=True)
+
+    def use_data_store(self, name: str, create: bool = True) -> Datastore:
         """Creates a datastore with the given name."""
         histories = self.galaxy_instance.histories.get_histories(name=name)
         if len(histories) > 0:
             store = Datastore(name, self, histories[0]["id"])
             self.datastores.append(store)
             return store
-        history_id = self.galaxy_instance.histories.create_history(name=name)["id"]
-        store = Datastore(name, self, history_id)
-        self.datastores.append(store)
-        return store
+        if create:
+            history_id = self.galaxy_instance.histories.create_history(name=name)["id"]
+            store = Datastore(name, self, history_id)
+            self.datastores.append(store)
+            return store
+        else:
+            raise Exception("Data store does not exist and auto creation is set to false.")
 
     def remove_data_store(self, store: Datastore) -> None:
         """Permanently deletes the data store with the given name."""
-        history = self.galaxy_instance.histories.get_histories(name=store.name)[0]["id"]
-        self.galaxy_instance.histories.delete_history(history_id=history, purge=True)
+        store.cleanup()
+        self.datastores.remove(store)
+
+    def close(self) -> None:
+        # Remove all data stores after execution
+        for store in self.datastores:
+            if not store.persist_store:
+                stop_all_tools_in_store(store)
+                self.remove_data_store(store)
 
 
 class Connection:
@@ -78,8 +99,15 @@ class Connection:
         self.galaxy_api_key = galaxy_key
         self.galaxy_instance: galaxy.GalaxyInstance
 
-    @contextmanager
-    def connect(self) -> Generator:
+    def _init_galaxy_instance(self) -> None:
+        if not self.galaxy_url or not self.galaxy_api_key:
+            raise ValueError("Galaxy URL and API key must be provided or set in environment variables.")
+        if not isinstance(self.galaxy_url, str):
+            raise ValueError("Galaxy URL must be a string")
+        self.galaxy_instance = galaxy.GalaxyInstance(url=self.galaxy_url, key=self.galaxy_api_key)
+        self.galaxy_instance.config.get_version()
+
+    def connect(self) -> ConnectionHelper:
         """
         Connects to the Galaxy instance using the provided URL and API key.
 
@@ -89,16 +117,6 @@ class Connection:
         ------
             ValueError: If the Galaxy URL or API key is not provided.
         """
-        if not self.galaxy_url or not self.galaxy_api_key:
-            raise ValueError("Galaxy URL and API key must be provided or set in environment variables.")
-        if not isinstance(self.galaxy_url, str):
-            raise ValueError("Galaxy URL must be a string")
-        self.galaxy_instance = galaxy.GalaxyInstance(url=self.galaxy_url, key=self.galaxy_api_key)
-        self.galaxy_instance.config.get_version()
+        self._init_galaxy_instance()
         conn = ConnectionHelper(self.galaxy_instance, self.galaxy_url)
-        yield conn
-        # Remove all data stores after execution
-        for store in conn.datastores:
-            if not store.persist_store:
-                stop_all_tools_in_store(store)
-                conn.remove_data_store(store)
+        return conn
